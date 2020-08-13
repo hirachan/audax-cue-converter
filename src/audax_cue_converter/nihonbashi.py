1add134365dd9a6a7e25143bb7eff6cc7a22446f
hirachan <yo@hirano.cc>
2020-08-13 18:24:56 +0900

    set style

--- src/audax_cue_converter/nihonbashi.py
+++ src/audax_cue_converter/nihonbashi.py
@@ -1,7 +1,7 @@
 from typing import List
 
 from openpyxl import Workbook
-from openpyxl.styles import PatternFill, Border, Font, Side
+from openpyxl.styles import PatternFill, Border, Font, Side, Alignment
 from openpyxl.formatting.rule import FormulaRule
 
 try:
@@ -17,6 +17,7 @@ class Nihonbashi:
         ws.title = "Cue"
 
         font8 = Font(name='メイリオ', size=8)
+        font11 = Font(name='メイリオ', size=11)
         font12 = Font(name='メイリオ', size=12)
 
         purple_fill = PatternFill(start_color='CCCCFF', end_color='CCCCFF', fill_type='solid')
@@ -68,9 +69,26 @@ class Nihonbashi:
             ws[f"K{row}"] = cue.comment
             ws[f"L{row}"] = cue.src
 
+        align1 = Alignment(horizontal="left", vertical="top", wrap_text=True, shrink_to_fit=False)
+        align2 = Alignment(horizontal="center", vertical="center", wrap_text=True, shrink_to_fit=False)
+        align3 = Alignment(horizontal="center", vertical="center", wrap_text=False, shrink_to_fit=True)
+        align4 = Alignment(horizontal="center", vertical="center", wrap_text=False, shrink_to_fit=False)
+
         for row in ws["A3":f"K{len(cues) + 3}"]:
             for cell in row:
-                ws[cell.coordinate].font = font12
+                if cell.column in (7, 9, 11):  # G, I, K
+                    ws[cell.coordinate].font = font11
+                else:
+                    ws[cell.coordinate].font = font12
+
+                if cell.column == 11:  # K
+                    ws[cell.coordinate].alignment = align1
+                elif cell.column in (9, 10):  # I, J
+                    ws[cell.coordinate].alignment = align2
+                elif cell.column == 7:  # G
+                    ws[cell.coordinate].alignment = align3
+                else:
+                    ws[cell.coordinate].alignment = align4
 
         # Write border
         thin = Side(border_style="thin", color="000000")
@@ -78,5 +96,17 @@ class Nihonbashi:
         for row in ws["A2":f"K{len(cues) + 3}"]:
             for cell in row:
                 ws[cell.coordinate].border = thin_border
+
+        ws.column_dimensions["A"].width = 7
+        ws.column_dimensions["B"].width = 9.5
+        ws.column_dimensions["C"].width = 9.5
+        ws.column_dimensions["D"].width = 9.5
+        ws.column_dimensions["E"].width = 6.4
+        ws.column_dimensions["F"].width = 4.5
+        ws.column_dimensions["G"].width = 16
+        ws.column_dimensions["H"].width = 4.5
+        ws.column_dimensions["I"].width = 14
+        ws.column_dimensions["J"].width = 11
+        ws.column_dimensions["K"].width = 36
 
         wb.save(filepath)
